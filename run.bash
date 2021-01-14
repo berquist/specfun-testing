272b5a6944a9b0f8c33ed58c0f7fc8792cfce6d8
Eric Berquist <eric.berquist@gmail.com>
2021-01-14 15:30:55 -0800

    add more clangs on coreman4

--- run.bash
+++ run.bash
@@ -19,7 +19,7 @@ standards=(98 11 17)
 if [[ "$HOST" == "dellman4" ]]; then
     modules=(intel/17.0.5 gnu/5.4.0 gnu/7.2.0 gnu/7.4.0 gnu/9.1.0 gnu/10.1.0)
 elif [[ "$HOST" == "coreman4" ]]; then
-    modules=(llvm-11.0.0-gcc-7.5.0-lix6xtm intel-oneapi-compilers-2021.1.0-gcc-9.3.0-4zfjnvr)
+    modules=(llvm-7.1.0-gcc-9.3.0-lutt6le llvm-9.0.1-gcc-9.3.0-5f5oyzd llvm-10.0.1-gcc-9.3.0-e64v527 llvm-11.0.0-gcc-7.5.0-lix6xtm intel-oneapi-compilers-2021.1.0-gcc-9.3.0-4zfjnvr)
 elif [[ "$HOST" == "osmium" ]]; then
     modules=(gcc-5.5.0-gcc-10.2.0-b2izpgj gcc-9.2.0-gcc-8.4.0-fvtn24p gcc-9.3.0-gcc-10.1.0-vptvs3i llvm-11.0.1-gcc-10.2.0-hbzie7q aocc-2.3.0-gcc-10.2.0-3y5aifd)
 else
